0b3370ed762418ce422027419b61a359828e9a3f
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2022-01-05 12:31:32 +0800

    Cam setting

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,29 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using MLAPI;
 
 public class GameManager : NetworkBehaviour
 {
-    public GameObject singlePlayerUI;
     public GameObject multiPlayerUI;
 
+    public Animator roundCamAnim;
+    public CinemachineVirtualCamera roundCam;
+
     private void Start() {
-        if (MainMenuController.isSingle) singlePlayerUI.SetActive(true);
-        else multiPlayerUI.SetActive(true);
+        multiPlayerUI.SetActive(true);
     }
 
     public void Game_Host() {
         NetworkManager.Singleton.StartHost();
 
-        if (MainMenuController.isSingle) singlePlayerUI.SetActive(false);
-        else multiPlayerUI.SetActive(false);
+        multiPlayerUI.SetActive(false);
+
+        roundCamAnim.SetTrigger("Start");
     }
 
     public void Game_Client() {
         NetworkManager.Singleton.StartClient();
 
-        if (MainMenuController.isSingle) singlePlayerUI.SetActive(false);
-        else multiPlayerUI.SetActive(false);
+        multiPlayerUI.SetActive(false);
+
+        roundCamAnim.SetTrigger("Start");
+    }
+
+    private void Update() {
+        if(roundCam.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition >= 20) {
+            roundCam.Priority = 0;
+        }      
+
+        // if(NetworkManager.)
     }
 }
